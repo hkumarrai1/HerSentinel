32f9b59aff6b69c0fe9ca5517c2b27ecf1460986
hkumarrai1 <166382429+hkumarrai1@users.noreply.github.com>
2026-04-23 21:14:17 +0530

    Update production API URL to hersentinel.onrender.com

--- HerSentinel/src/config/api.js
+++ HerSentinel/src/config/api.js
@@ -8,7 +8,7 @@ const ENV = {
     API_BASE_URL: 'http://192.168.1.34:4000/api', // Update this when laptop WiFi IP changes
   },
   prod: {
-    API_BASE_URL: 'https://api.hersentinel.com/api', // Production backend URL
+    API_BASE_URL: 'https://hersentinel.onrender.com/api', // Production backend URL
   },
 };
 
